1845168c7f749372c273e2d69faf7c302a003d43
Paula <hulabg@hotmail.com>
2015-11-20 17:46:01 +0100

    detalle post

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+
+def post_detail(request, pk):
+    #post = Post.objects.get(pk=pk)
+
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
