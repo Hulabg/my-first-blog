b3e11b792738cc705dc61e1bd8c0587bdd4d8ef2
Paula <hulabg@hotmail.com>
2015-11-20 18:32:26 +0100

    formulario

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,8 @@
 # Create your views here.
 from django.utils import timezone
 from .models import Post
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import PostForm
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -15,3 +16,16 @@ def post_detail(request, pk):
 
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+        if request.method == "POST":
+            form = PostForm(request.POST)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.author = request.user
+                post.publish()
+                post.save()
+                return redirect('blog.views.post_detail', pk=post.pk)
+        else:
+            form = PostForm()
+        return render(request, 'blog/post_edit.html', {'form': form})
